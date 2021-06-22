bf26a6aa7045b22e8a1cdcb92bbe958ba90c4f26
Bruce <leeharrison1984@gmail.com>
2021-06-22 12:01:14 -0500

    add help text to extension methods

--- TinyHealthCheck/Extensions.cs
+++ TinyHealthCheck/Extensions.cs
@@ -7,16 +7,35 @@ namespace TinyHealthCheck
 {
     public static class Extensions
     {
+        /// <summary>
+        /// Adds a simple health check endpoint that returns a JSON payload
+        /// </summary>
+        /// <param name="services">Service Collection</param>
+        /// <param name="configFunc">Custom TinyHealthCheck configuration object</param>
+        /// <returns>Service Collection</returns>
         public static IServiceCollection AddBasicTinyHealthCheck(this IServiceCollection services, Func<TinyHealthCheckConfig, TinyHealthCheckConfig> configFunc)
         {
             return services.AddCustomTinyHealthCheck<BasicHealthCheck>(configFunc);
         }
 
+        /// <summary>
+        /// Adds a simple health check endpoint that returns a JSON payload with an Uptime counter
+        /// </summary>
+        /// <param name="services">Service Collection</param>
+        /// <param name="configFunc">Custom TinyHealthCheck configuration object</param>
+        /// <returns>Service Collection</returns>
         public static IServiceCollection AddBasicTinyHealthCheckWithUptime(this IServiceCollection services, Func<TinyHealthCheckConfig, TinyHealthCheckConfig> configFunc)
         {
             return services.AddCustomTinyHealthCheck<BasicHealthCheckWithUptime>(configFunc);
         }
 
+        /// <summary>
+        /// Adds a custom health check endpoint that return any type of payload
+        /// </summary>
+        /// <typeparam name="T">The custom health check type</typeparam>
+        /// <param name="services">Service Collection</param>
+        /// <param name="configFunc">Custom TinyHealthCheck configuration object</param>
+        /// <returns>Service Collection</returns>
         public static IServiceCollection AddCustomTinyHealthCheck<T>(this IServiceCollection services, Func<TinyHealthCheckConfig, TinyHealthCheckConfig> configFunc) where T : IHealthCheck
         {
             return services.AddSingleton<IHostedService>(x =>
