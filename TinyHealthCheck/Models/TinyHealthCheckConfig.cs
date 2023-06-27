10c61a924bd496f84d7b5bbe16d71e905d83198b
Twinki <thetwinkehh@gmail.com>
2023-06-26 20:26:12 -0400

    Update TinyHealthCheckConfig.cs

--- TinyHealthCheck/Models/TinyHealthCheckConfig.cs
+++ TinyHealthCheck/Models/TinyHealthCheckConfig.cs
@@ -6,11 +6,12 @@ namespace TinyHealthCheck.Models
     {
         /// <summary>
         /// The hostname that will be listened on. 'localhost' by default, typically '*' is desired unless you know the hostname ahead of time.
+        /// If running inside a docker container, * or + may need to be used instead of 'localhost'.
         /// </summary>
         public string Hostname { get; set; } = "localhost";
 
         /// <summary>
-        /// Port that this health check will be served on
+        /// Port that this health check will be served on.
         /// </summary>
         public int Port { get; set; } = 8080;
 
